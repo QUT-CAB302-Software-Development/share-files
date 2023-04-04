f81ae0b2822e9d9b28e1e2f84c49733a2009fa15
Dan Tran <autumnssuns@gmail.com>
2023-04-04 21:52:15 +1000

    Added example.data package

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# share-files
-A public repository that can be used to share groups of files, using the different branches
+# JUnit5 Tests for project template
+
+This branch contains the `example.data` package with the following classes:
+
+- `DBConnection.java`
+- `InvalidUserException.java`
+- `StaticDatabaseConnection.java`
+- `StaticUserDAO.java`
+- `User.java`
+- `UserDAO.java`
+
+These classes mocks the behaviour of a database connection and a user DAO by using a static ArrayList of users as a database.
+
+The tests for these classes can be downloaded [here](https://github.com/QUT-CAB302-Software-Development/share-files/archive/junit-data-tests.zip).
---
+++ User.java
@@ -0,0 +1,83 @@
+package example.data;
+
+/**
+ * Represents a user of the application.
+ */
+public class User {
+    private String name;
+    private String password;
+    private String email;
+
+    /**
+     * Creates a new User.
+     *
+     * @param name     The name of the user.
+     * @param password The password of the user.
+     * @param email    The email of the user.
+     */
+    public User(String name, String password, String email) throws InvalidUserException {
+        this.name = name;
+        setPassword(password);
+        setEmail(email);
+    }
+
+    /**
+     * Gets the name of the user.
+     *
+     * @return The name of the user.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the password of the user.
+     *
+     * @return The password of the user.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Gets the email of the user.
+     *
+     * @return The email of the user.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the name of the user.
+     *
+     * @param newName The new name of the user.
+     */
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setPassword(String password) throws InvalidUserException {
+        if (password.length() < 5) {
+            throw new InvalidUserException("Password must be at least 5 characters long.");
+        }
+        if (!password.matches(".*[0-9].*")) {
+            throw new InvalidUserException("Password must contain at least one number.");
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            throw new InvalidUserException("Password must contain at least one uppercase letter.");
+        }
+        if (!password.matches(".*[a-z].*")) {
+            throw new InvalidUserException("Password must contain at least one lowercase letter.");
+        }
+        this.password = password;
+    }
+
+    public void setEmail(String email) throws InvalidUserException {
+        String[] parts = email.split("@");
+        if (parts.length != 2) {
+            throw new InvalidUserException("Email must contain only one @ symbol.");
+        }
+        this.email = email;
+    }
+}
